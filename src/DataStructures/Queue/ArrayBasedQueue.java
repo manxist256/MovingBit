a48a80680bac022936a6f7cfb3d345cb8810ae3b
Kumaran Babu Kuttuva <mankank@amazon.com>
2018-11-17 20:14:25 +0530

    Queue Implementations

--- src/DataStructures/Queue/ArrayBasedQueue.java
+++ src/DataStructures/Queue/ArrayBasedQueue.java
@@ -5,10 +5,88 @@
  */
 package DataStructures.Queue;
 
+//import java.util.Scanner;
+
+import java.util.Arrays;
+
+
 /**
  *
  * @author mankank
  */
-public class ArrayBasedQueue {
+
+class Queue { 
+    int[] queue;
+    int front;
+    int rear;
     
+    public Queue(int queue_size) {
+        this.queue = new int[queue_size];
+    }
+    
+    public void enQueue(int data) {
+        try {
+            queue[rear++] = data;
+        } catch(Exception e) {
+            System.out.println("Queue full!");
+        }
+    }
+    
+    public void deQueue() {
+        if (rear == 0) {
+            System.out.println("Queue empty!");
+            return;
+        }
+        front++;
+        if (front == rear) {
+            resetQueue();
+        } 
+    }
+    
+    private void resetQueue() {
+        this.front = 0;
+        this.rear = 0;
+        Arrays.fill(queue, 0);
+    }
+    
+    public void displayQueue() {
+        for (int i = front; i < rear; i++) {
+            System.out.print(queue[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public void queueSize() {
+        System.out.println(rear - front);
+    }
+    
+    public void getFront() {
+        System.out.println(queue[front]);
+    }
+}
+
+public class ArrayBasedQueue {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        Queue queue = new Queue(5);
+        queue.enQueue(1);
+        queue.enQueue(45);
+        queue.displayQueue();
+        queue.queueSize();
+        /*queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+        queue.enQueue(4);
+        queue.enQueue(5);
+        queue.displayQueue();
+        /*queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();*/
+        //queue.displayQueue();*/
+        //queue.getFront();
+        
+    }
 }
--- src/DataStructures/Queue/LinkedListBasedQueue.java
+++ src/DataStructures/Queue/LinkedListBasedQueue.java
@@ -9,6 +9,130 @@ package DataStructures.Queue;
  *
  * @author mankank
  */
-public class LinkedListBasedQueue {
+
+class Node {
+    int data;
+    Node next;
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
+class LinkedList {
+    Node last, head;
+    public LinkedList() {
+        this.last = null;
+        this.head = null;
+    }
     
+    public void insertAtLast(int data) {
+        Node newnode = new Node(data);
+        if (this.last == null) {
+            this.last = newnode;
+            this.head = newnode;
+            return;
+        }
+        last.next = newnode;
+        last = newnode;
+    }
+    
+    public void deleteAtFirst() throws Exception {
+        if (this.head == null) {
+            throw new Exception();
+        }
+        this.head = head.next;
+        if (this.head == null) {
+            this.last = null;
+        }
+    }
+    
+    public int getHead() throws Exception {
+        if (this.head == null) {
+            throw new Exception();
+        }
+        return this.head.data;
+    }
+    
+    public void traverse() {
+        Node traverse = this.head;
+        while (traverse != null) {
+            System.out.print(traverse.data + " ");
+            traverse = traverse.next;
+        }
+        System.out.println();
+    }
+ 
+    public int getLLSize() {
+        int size = 0;
+        Node traverse = this.head;
+        while (traverse != null) {
+            size++;
+            traverse = traverse.next;
+        }
+        return size;
+    }
+    
+}
+
+class LLQueue {
+    LinkedList queue;
+    
+    public LLQueue() {
+        this.queue = new LinkedList();
+    }
+    
+    public void enQueue(int data) {
+        this.queue.insertAtLast(data);
+    }
+    
+    public void deQueue() {
+        try {
+            this.queue.deleteAtFirst();
+        } catch(Exception e) {
+            System.out.println("Queue empty");
+        }
+    }
+    
+    public void getFront() {
+        try {
+            int head = this.queue.getHead();
+            System.out.println(head + " : HEAD");
+        } catch(Exception e) {
+            System.out.println("Queue empty!");
+        }
+    }
+    
+    public void displayQueue() {
+        this.queue.traverse();
+    }
+    
+    public void queueSize() {
+        int size = this.queue.getLLSize();
+        System.out.println(size + " : Size");
+    }
+    
+}
+
+public class LinkedListBasedQueue {
+    public static void main(String[] args) {
+        LLQueue queue = new LLQueue();
+        queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+        queue.enQueue(4);
+        queue.enQueue(5);
+        queue.queueSize();
+        queue.displayQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.displayQueue();
+        queue.enQueue(45);
+        queue.enQueue(93);
+        queue.displayQueue();
+        queue.queueSize();
+        queue.getFront();
+    }
 }
