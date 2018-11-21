121f228c82ac173e9d73b109371da828e016427b
Kumaran Babu Kuttuva <mankank@amazon.com>
2018-11-21 12:08:20 +0530

    Adding implementation for FCFS

--- src/Algorithms/CPUScheduling/FCFS.java
+++ src/Algorithms/CPUScheduling/FCFS.java
@@ -5,10 +5,28 @@
  */
 package Algorithms.CPUScheduling;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mankank
  */
 public class FCFS {
-    
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int N = sc.nextInt();
+                int[] burst_time = new int[N]; int s = 0;
+                int[] service_time = new int[N];
+                int[] completion_time = new int[N];
+                for (int i = 0; i < N; i++) {
+                        service_time[i] = s;
+                        burst_time[i] = sc.nextInt();
+                        s += burst_time[i];
+                        completion_time[i] = s;
+                }
+                System.out.println("Process   BurstTime ServiceTime CompletionTime");
+                for (int i = 0; i < service_time.length; i++) {
+                        System.out.println(i + "         " + burst_time[i] + "         " + service_time[i] + "           " + completion_time[i]);
+                }
+        }
 }
