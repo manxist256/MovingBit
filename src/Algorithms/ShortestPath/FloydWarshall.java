b849de4e2a78e10b799e83c81f20449688ada587
Kumaran Babu Kuttuva <mankank@amazon.com>
2018-11-24 19:20:44 +0530

    Adding floyd warshall implementation

--- src/Algorithms/ShortestPath/FloydWarshall.java
+++ src/Algorithms/ShortestPath/FloydWarshall.java
@@ -5,11 +5,35 @@
  */
 package Algorithms.ShortestPath;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mankank
  */
 //ASSP
 public class FloydWarshall {
-    
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int N = sc.nextInt();
+                int adj_mat[][]  = new int[N][N];
+                for(int i = 0; i < N; i++) {
+                        for(int j = 0 ;j < N; j++)
+                                adj_mat[i][j]=sc.nextInt();
+                }
+                for(int k = 0; k < N; k++) {
+                        for(int i = 0;i < N; i++){
+                                for(int j = 0; j < N; j++){
+                                    if(adj_mat[i][j] > adj_mat[i][k] + adj_mat[k][j])
+                                        adj_mat[i][j] = adj_mat[i][k] + adj_mat[k][j];
+                                }
+                        }
+                }
+                for(int i = 0; i < N; i++) {
+                        for(int j = 0; j < N; j++){
+                                System.out.print(adj_mat[i][j]+" ");
+                        }
+                        System.out.println();
+                }
+        }
 }
