412c51e548d2f6b3bbd4d407d4f8d74ba36a4191
Kumaran Babu Kuttuva <mankank@amazon.com>
2018-11-26 19:57:27 +0530

    Learning about File and FileWriter

---
+++ src/File/FileWriter_.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package File;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author mankank
+ */
+public class FileWriter_ {
+        public static void main(String[] args) throws IOException {
+                FileWriter fw = new FileWriter("a.txt", true); //pass append = true, else filewriter constructor will delete the older file named a.txt and creates new a.txt.
+                fw.write("Appending new characters to existing file...");
+                fw.close();
+        }
+}
