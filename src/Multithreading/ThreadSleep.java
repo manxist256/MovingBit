7f2319db14f8422ce7c822aa8746e7be6253083a
Kumaran Babu Kuttuva <mankank@amazon.com>
2018-12-03 22:41:04 +0530

    Learnings about Thread.yield and Thread.sleep

--- src/Multithreading/ThreadSleep.java
+++ src/Multithreading/ThreadSleep.java
@@ -5,10 +5,44 @@
  */
 package Multithreading;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mankank
  */
-public class ThreadSleep {
+
+class SThread extends Thread {
+
+        public SThread(String name) {
+                super(name);
+        }
     
+        @Override
+        public void run() {
+                for (int i = 0; i < 10; i++) {
+                        if (Thread.currentThread().getName().equals("T1")) {
+                            try {
+                                Thread.sleep(2000); //sleeps the current executing thread {name : T1} and moves to waiting state for 2 secs
+                                // Thread.sleep immediately captures the current executing thread and put only that thread to sleep.
+                                //Thread.currentThread().interrupt();
+                            } catch (InterruptedException ex) {
+                                Logger.getLogger(SThread.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                        System.out.println(Thread.currentThread().getName() + " " + i);
+                }
+        }
+}
+
+public class ThreadSleep {
+        public static void main(String[] args) {
+                SThread t1 = new SThread("T1");
+                SThread t2 = new SThread("T2");
+                SThread t3 = new SThread("T3");
+                t1.start();
+                t2.start();
+                t3.start();
+        }
 }
--- src/Multithreading/ThreadYield.java
+++ src/Multithreading/ThreadYield.java
@@ -9,6 +9,31 @@ package Multithreading;
  *
  * @author mankank
  */
-public class ThreadYield {
+
+class YThread extends Thread {
     
+        public YThread(String name) {
+                super(name);
+        }
+    
+        @Override
+        public void run() {
+                for (int i = 0; i < 5; i++) {
+                        System.out.println(Thread.currentThread().getName() + " " + i);
+                        if (Thread.currentThread().getName().equals("T1")) {
+                                Thread.yield(); //yields the current executing thread and moves to waiting state
+                        }
+                }
+        }
+}
+
+public class ThreadYield {
+        public static void main(String[] args) {
+                YThread t1 = new YThread("T1");
+                YThread t2 = new YThread("T2");
+                YThread t3 = new YThread("T3");
+                t1.start();
+                t2.start();
+                t3.start();
+        }
 }
