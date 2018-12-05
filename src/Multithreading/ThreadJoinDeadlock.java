7dcb2670d14c24f35068299dacfed7e6d03ef7d2
Kumaran Babu Kuttuva <mankank@amazon.com>
2018-12-05 11:18:34 +0530

    Learnings about Thread.join and deadlock situation

--- src/Multithreading/ThreadJoinDeadlock.java
+++ src/Multithreading/ThreadJoinDeadlock.java
@@ -14,11 +14,11 @@ import java.util.logging.Logger;
  * @author mankank
  */
 
-class TThread extends Thread {
+class TThreadDL extends Thread {
     
         static ArrayList<Thread> threadGroup = new ArrayList();
     
-        public TThread(ThreadGroup gp, String name) {
+        public TThreadDL(ThreadGroup gp, String name) {
                 super(gp, name);
         }
     
@@ -43,18 +43,32 @@ class TThread extends Thread {
                                         }
                                 }
                         }
+                        if (Thread.currentThread().getName().equals("T2")) {
+                                ThreadGroup group = Thread.currentThread().getThreadGroup();
+                                for (Thread thread : threadGroup) {
+                                        if (thread.getName().equals("T1")) {
+                                                try {
+                                                        thread.join(); // internally t1.join(); // then t2 waits until execution of t1 completes.
+                                                } catch (InterruptedException ex) {
+                                                        Logger.getLogger(TThread.class.getName()).log(Level.SEVERE, null, ex);
+                                                }
+                                                break;
+                                        }
+                                }
+                        }
+                        // so both thread will keep on waiting until each other's completion. Hence the program will never stop because of deadlock situation.
                         System.out.println(Thread.currentThread().getName());
                 }
         }
 }
 
-public class ThreadJoin {
+public class ThreadJoinDeadlock {
         public static void main(String[] args) {
                ThreadGroup group = new ThreadGroup("TG1");
-               TThread t1 = new TThread(group, "T1"); 
-               TThread t2 = new TThread(group, "T2");
-               TThread.threadGroup.add(t1);
-               TThread.threadGroup.add(t2);
+               TThreadDL t1 = new TThreadDL(group, "T1"); 
+               TThreadDL t2 = new TThreadDL(group, "T2");
+               TThreadDL.threadGroup.add(t1);
+               TThreadDL.threadGroup.add(t2);
                t1.start();
                t2.start();
         }
