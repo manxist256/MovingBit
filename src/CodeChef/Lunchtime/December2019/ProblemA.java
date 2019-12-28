b41e406822c3dbac0e885d032e051c13f3d6f34e
manikandankk <manikandan.kk@flipkart.com>
2019-12-29 00:20:40 +0530

    Lunchtime 2019 Dec CC

---
+++ src/CodeChef/Lunchtime/December2019/ProblemA.java
@@ -0,0 +1,151 @@
+package CodeChef.Lunchtime.December2019;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+public class ProblemA {
+
+    static ReaderWriter rw = new ReaderWriter();
+    static Helpers hp = new Helpers();
+
+    private static void solve(boolean multipleTC) throws Exception {
+        int T = multipleTC ? rw.ri() : 1;
+        while (T-- > 0) {
+            String s = rw.rs();
+            long c = 0;
+            long started = s.length() - 1;
+            long jj = ((long)(s.length()) * (long)(s.length() + 1)) / 2;
+            for (int i = 0; i < s.length() - 1; i++) {
+                int n1 = s.charAt(i);
+                int n2 = s.charAt(i+1);
+                long total = (started * (i+1)) + started + i + 1;
+                long rem = jj - total;
+                if (n1 == n2) {
+                    c += (started * (i+1));//both switches
+                    c += rem; //both not switches
+                }
+                if (n1 != n2) {
+                    c += started; //next guy switches and current guy not
+                    c += (i+1);//current guy switches and next guy not
+                }
+
+
+                started--;
+            }
+            rw.println(c);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        solve(true);
+        rw.flush();
+    }
+
+    static class Helpers {
+
+        static long gcd(long a, long b) {
+            if (b == 0) {
+                return a;
+            }
+            return gcd(b, a % b);
+        }
+
+        static long lcm(long a, long b) {
+            return (a / gcd(a, b)) * b;
+        }
+
+        static <T> HashMap<T, Integer> freqMap(T[] array) {
+            HashMap<T, Integer> map = new HashMap<>();
+            for (int i = 0; i < array.length; i++) {
+                if (!map.containsKey(array[i])) {
+                    map.put(array[i], 0);
+                }
+                int ev = map.get(array[i]);
+                map.put(array[i], ++ev);
+            }
+            return map;
+        }
+
+    }
+
+    static class ReaderWriter {
+        static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        static PrintWriter printWriter = new PrintWriter(System.out);
+
+        protected void flush() throws Exception {
+            printWriter.flush();
+            printWriter.close();
+            bufferedReader.close();
+        }
+
+        protected void println(String s) {
+            printWriter.println(s);
+        }
+
+        protected void println(int i) {
+            printWriter.println(i);
+        }
+
+        protected void println(long l) {
+            printWriter.println(l);
+        }
+
+        int ri() throws Exception {
+            return Integer.parseInt(bufferedReader.readLine());
+        }
+
+        long rl() throws Exception {
+            return Long.parseLong(bufferedReader.readLine());
+        }
+
+        String rs() throws Exception {
+            return bufferedReader.readLine();
+        }
+
+        double rd() throws Exception {
+            return Double.parseDouble(bufferedReader.readLine());
+        }
+
+        int[] ria() throws Exception {
+            String[] line = rs().split(" ");
+            int[] arr = new int[line.length];
+            for (int i = 0; i < line.length; i++) {
+                arr[i] = Integer.parseInt(line[i]);
+            }
+            return arr;
+        }
+
+        long[] rla() throws Exception {
+            String[] line = rs().split(" ");
+            long[] arr = new long[line.length];
+            for (int i = 0; i < line.length; i++) {
+                arr[i] = Integer.parseInt(line[i]);
+            }
+            return arr;
+        }
+
+        int gifs(int[] line, int index) {
+            return line[index];
+        }
+
+        long glfs(long[] line, int index) {
+            return line[index];
+        }
+
+        void pia(int[] array) {
+            for (int i = 0; i < array.length; i++) {
+                printWriter.print(array[i] + " ");
+            }
+            printWriter.println();
+        }
+
+        void pla(long[] array) {
+            for (int i = 0; i < array.length; i++) {
+                printWriter.print(array[i] + " ");
+            }
+            printWriter.println();
+        }
+    }
+}
